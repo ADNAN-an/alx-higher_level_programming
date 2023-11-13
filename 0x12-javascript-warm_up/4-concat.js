de3766d8afbdc158b4d18b4d7b5427d32fcad49b
adnan <adnan.anjar2002@gmail.com>
2023-11-13 10:17:18 -0800

    1 to 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv[2] && process.argv[3]) {
-  console.log(process.argv[2] + " is " + process.argv[3]);
-} else {
-  console.log('No argument');
-}
+const myVar1 = process.argv[2];
+const myVar2 = process.argv[3];
+
+const result = `${myVar1} is ${myVar2}`;
+
+console.log(result);
